9d1f02492e937e78385f6feb5b09d2eb9436b0be
Shivam_Singh <shivamsingh6490@gmail.com>
2026-05-08 22:49:14 +0530

    how to convert object to json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const json='{"name":"Shivam","age":23,"city":"Siwan"}';
-const obj=JSON.parse(json);
-console.log(obj.name);
+const json={
+    name:"Shivam",
+    age:23,
+    city:"Siwan"
+};
+const obj=JSON.stringify(json);
+console.log(obj);
