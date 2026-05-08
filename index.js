5bc440ab8a2b9aff831ce3e35954c25320f512b1
Shivam_Singh <shivamsingh6490@gmail.com>
2026-05-08 22:46:16 +0530

    how to convert json to object

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-import fs from "fs";
-import os from "os";
-import _ from "lodash";
-
-console.log(os.userInfo());
-const user=os.userInfo();
-console.log(user.username);
-
-
-// fs.appendFile("greeting.txt"," Hi "+user.username+"\n",()=>console.log("your file created"));
-const getData=fs.readFile("greeting.txt",()=>console.log('we got the file!'));
-console.log(getData);
-
-const data=["Shivam","Shivam",7,8,7,8,9,true];
-console.log(_.uniq(data));
+const json='{"name":"Shivam","age":23,"city":"Siwan"}';
+const obj=JSON.parse(json);
+console.log(obj.name);
