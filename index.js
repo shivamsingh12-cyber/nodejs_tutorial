9953079a16ab78107f5482247c638a116b81d0df
Shivam_Singh <shivamsingh6490@gmail.com>
2026-05-16 23:51:53 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app=express();
 
@@ -6,6 +7,20 @@ app.get("/",(req,res)=>{
     res.send("you got it! man");
 })
 
+const mongoUrl="mongodb://localhost:27017/hotels";
+
+mongoose.connect(mongoUrl).then(()=>console.log('You are connected to database!')).catch((err)=>{console.log(err)});
+
+mongoose.Schema({
+    name:String,
+    city:String,
+    age:Number
+})
+
+app.post("/save",(req,res)=>)
+
+
 app.listen(3000,()=>{
     console.log('Your server is running');
 })
+
